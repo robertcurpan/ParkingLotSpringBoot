fbad2e34327506a0329e2effd148c77f9432a545
Robert Curpan <robert.curpan@basware.com>
2022-11-08 10:42:06 +0200

    New IP for ec2 instance

--- src/main/java/com/robert/ParkingLot/controller/ParkingLotController.java
+++ src/main/java/com/robert/ParkingLot/controller/ParkingLotController.java
@@ -28,7 +28,7 @@ public class ParkingLotController {
     public AuthenticationService authenticationService;
 
     @PostMapping(value = "/generateParkingTicket")
-    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://52.50.172.119"})
+    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://34.243.149.64"})
     public ResponseEntity<Ticket> generateParkingTicket(@RequestBody VehicleJson vehicleJson) throws ParkingLotGeneralException {
         Vehicle vehicle = CreateVehicleFromJsonUtil.createVehicle(vehicleJson);
         Ticket ticket = parkingLotService.generateParkingTicket(vehicle);
@@ -36,28 +36,28 @@ public class ParkingLotController {
     }
 
     @PostMapping(value = "/leaveParkingLot")
-    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://52.50.172.119"})
+    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://34.243.149.64"})
     public ResponseEntity<Ticket> leaveParkingLot(@RequestBody ParkingSpot parkingSpot) throws ParkingLotGeneralException {
         Ticket ticket = parkingLotService.leaveParkingLot(parkingSpot.getId());
         return new ResponseEntity<Ticket>(ticket, HttpStatus.OK);
     }
 
     @GetMapping(value = "/getParkingSpots")
-    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://52.50.172.119"})
+    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://34.243.149.64"})
     public ResponseEntity<List<ParkingSpot>> getParkingSpots() {
         List<ParkingSpot> parkingSpots = parkingLotService.getAllParkingSpots();
         return new ResponseEntity<List<ParkingSpot>>(parkingSpots, HttpStatus.OK);
     }
 
     @GetMapping(value = "/getTickets")
-    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://52.50.172.119"})
+    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://34.243.149.64"})
     public ResponseEntity<List<Ticket>> getTickets() {
         List<Ticket> tickets = parkingLotService.getTickets();
         return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
     }
 
     @PostMapping(value = "/login")
-    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://52.50.172.119"})
+    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://34.243.149.64"})
     public ResponseEntity<AuthorizationResponse> login(@RequestBody UserDetails userDetails) throws ParkingLotGeneralException {
         String jwt = authenticationService.verifyLoginAndGenerateJwt(userDetails);
         User user = authenticationService.getUserByUsername(userDetails.getUsername());
@@ -66,7 +66,7 @@ public class ParkingLotController {
     }
 
     @PostMapping(value = "/register")
-    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://52.50.172.119"})
+    @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:4200", "http://34.243.149.64"})
     public ResponseEntity<StandardMessage> register(@RequestBody UserDetails userDetails) throws ParkingLotGeneralException {
         String hashedPassword = BCrypt.hashpw(userDetails.getPassword(), BCrypt.gensalt(10));
         authenticationService.registerUser(new User(userDetails.getUsername(), hashedPassword, "user"));
